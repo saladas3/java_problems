970ab33129c4cf8f16311169e83f447efcd7a262
saladas3 <lucaion.work@gmail.com>
2019-05-10 19:09:59 +0300

    bubble_sort

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("hello again");
+        int n;
+
+        Scanner in = new Scanner(System.in);
+
+        n = in.nextInt();
+
+        int[] vector = new int[n];
+        for (int i = 0; i < n; i++) {
+            vector[i] = in.nextInt();
+        }
+
+        boolean swapped = true;
+
+        while (swapped){
+            swapped = false;
+            for (int i = 0; i < n-1; i++) {
+                if (vector[i]>vector[i+1]){
+                    int aux;
+                    aux = vector[i+1];
+                    vector[i+1] = vector[i];
+                    vector [i] = aux;
+                    swapped = true;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(vector));
     }
 }
